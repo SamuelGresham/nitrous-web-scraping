c3ad1647682b89d0d3f522506a33057100c8a69a
Samuel Gresham <samuelgresham12@gmail.com>
2024-10-08 20:18:22 +1100

    Data collection

--- src/scraping/scrape_page.py
+++ src/scraping/scrape_page.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import pandas as pd 
 
 # Define globals
-domain = "https://www.creamify.com.au/"
+domain = "https://www.ezychargers.com.au/"
 internal_links = [domain]
 internal_links_searched = []
 
